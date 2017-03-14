8e93eb14c6c4aa9dc2826e97309f895506956a5f
Rodríguez Perez, Jhon <jrperea@e-externas.aena.es>
2017-03-14 23:02:26 +0000

    Cambio de idioma los del formulario

--- app/src/main/java/com/majada/jhona/myapplication/Principal.java
+++ app/src/main/java/com/majada/jhona/myapplication/Principal.java
@@ -1,33 +1,63 @@
 package com.majada.jhona.myapplication;
 
-import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
+
 
 
 public class Principal extends AppCompatActivity {
     private ListView listaTareas;
     ArrayList<Tarea> registros;
 
-    private Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
        listaTareas =(ListView) findViewById(R.id.lvTareas);
        AdminSQLite admin = new AdminSQLite(this,"ToDo", null, 1);
+        //Acciones sobre los elementos.
+        listaTareas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int posicion=i;
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(Principal.this);
+                dialogo1.setTitle("Importante");
+                dialogo1.setMessage("¿ Elimina este teléfono ?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        TextView date =(TextView) findViewById(R.id.fechaTarea);
+
+                        date.setText("lalalalal");
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                    }
+                });
+                dialogo1.show();
+
+                ///return false;
+            }
+        });
        SQLiteDatabase bd = admin.getWritableDatabase();
        Cursor fila = bd.rawQuery("select tarea, estado ,prioridad, fecha, hora from tareas ", null);
         registros = new ArrayList<Tarea>();
@@ -42,19 +72,23 @@ public class Principal extends AppCompatActivity {
         AdaptadorTareas adaptador = new AdaptadorTareas(this);
         ListView lv1 = (ListView)findViewById(R.id.lvTareas);
         lv1.setAdapter(adaptador);
-       // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, registros);
-
-
-        //setListAdapter(new Tarea(this, (Vector) fila));
-        //listaTareas.setAdapter(adapter);
         bd.close();
     }
-    /*registros.add( new Tarea(  )fila.getString(0)) + " prioridad: " +fila.getString(1) + " estado : "+
-                            fila.getString(2));*/
     //Abrir activity de añadir al realizar click en el footer
     public void add(View v){
         Intent i = new Intent(this, Add.class );
         startActivity(i);
+    }
+
+
+    public void notificacion(View v){
+        TextView fecha  = (TextView) findViewById(R.id.fechaTarea);
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!"+ fecha.getText().toString();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     class AdaptadorTareas  extends ArrayAdapter<Tarea>{
@@ -65,16 +99,17 @@ public class Principal extends AppCompatActivity {
             appCompatActivity = context;
         }
 
+
         public View getView(int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater = appCompatActivity.getLayoutInflater();
             View item = inflater.inflate(R.layout.tarea, null);
             TextView tituloTarea = (TextView)item.findViewById(R.id.tareaList);
             tituloTarea.setText(registros.get(position).getTitulo());
             TextView fechaTarea = (TextView)item.findViewById(R.id.fechaTarea);
-            fechaTarea.setText("Fecha: "+ registros.get(position).getFecha() +" "+registros.get(position).getHora());
+            fechaTarea.setText("Date: "+ registros.get(position).getFecha() +" "+registros.get(position).getHora());
             //Marco la priodidad de la tarea.
             TextView prioridadTarea = (TextView)item.findViewById(R.id.prioridadTarea);
-            prioridadTarea.setText("Prioridad: "+ registros.get(position).getPrioridad());
+            prioridadTarea.setText("Priority: "+ registros.get(position).getPrioridad());
             //Marco si está realizada o no dependiendo de la consulta.
             CheckBox checkTarea = (CheckBox) item.findViewById(R.id.checkTarea);
              if(registros.get(position).getEstado().equals("Done")){
@@ -82,8 +117,7 @@ public class Principal extends AppCompatActivity {
              }else{
                 checkTarea.setChecked(false);
              }
-             //Muestro la fecha
-            fechaTarea.setText("Fecha: "+ registros.get(position).getFecha() +" "+registros.get(position).getHora());
+
             return(item);
         }
 
